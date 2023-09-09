fee8c04f2de4054e2670871acf2532a6665472d8
Peter Houghton <pete@investigatingsoftware.co.uk>
2023-09-09 17:51:11 +0000

    text with white rectangle bg

--- qr_gen.py
+++ qr_gen.py
@@ -2,8 +2,12 @@ import qrcode
 from PIL import ImageDraw, ImageFont
 
 
+box_size = 100
 url = "https://github.com/phoughton"
-img = qrcode.make(url, box_size=10, border=2)
+img = qrcode.make(url,
+                  box_size=box_size,
+                  error_correction=qrcode.constants.ERROR_CORRECT_H,
+                  border=1)
 
 file_path = "github_qr_code.png"
 # img.save(file_path)
@@ -11,16 +15,19 @@ file_path = "github_qr_code.png"
 draw = ImageDraw.Draw(img)
 text = "Pete Houghton"
 
-font = ImageFont.truetype("DejaVuSans.ttf", 40)
+font = ImageFont.truetype("DejaVuSans.ttf", box_size*3.5)
 left, top, right, bottom = font.getbbox(text)
 
 text_height = abs(bottom - top)
 text_width = abs(right - left)
-# print(text_height, text_width)
-print(img.size[0], img.size[1])
+
 x = (img.size[1] - text_width) / 2
 y = (img.size[0] - text_height) / 2
-# print(x, y)
+draw.rectangle((x, y,
+                x + int(text_width*1.2),
+                y + int(text_height*1.2)),
+                fill="white")
+
 draw.text((x, y), text, font=font)
 
 img.save(file_path)
